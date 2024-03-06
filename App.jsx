9d77ada0f0aea50ec421630239a7445f2d1c7e45
Gurleenghotra <127692873+Gurleenghotra@users.noreply.github.com>
2024-03-05 21:01:29 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+import {loginGoogle, onSignOut, registroUsuario} from './Firebase/client'
+import useUsuairo from './hooks/useUsuario'
+
+function App() {
+  const usuario = useUsuairo()
+  const [isLogin, setIsLogin] = useState(true)
+  const [formData, setFormData] = useState({ email: '', pasword: ''})
+
+  const handleSubmit = () => {
+    if (isLogin) {
+      console.log("Iniciando sesión...", formData)
+    } else {
+      registroUsuario(formData.email, formData.pasword)
+    }
+  }
+
+  return (
+    <main>
+      {
+        usuario
+        ? <section>
+            <h1>Bienvenido {usuario}</h1>
+            <button onClick={ onSignOut }>Cerrar sesión</button>
+          </section>
+        : <section>
+            <h1>Firebase Auth</h1>
+            <input type="email" placeholder='Correo electrónico' value={formData.email} onChange={(e) => setFormData({...formData, email: e.target.value})}/>
+            <input type="password" placeholder='Contraseña' value={formData.pasword} onChange={(e) => setFormData({...formData, pasword: e.target.value})}/>
+            <button onClick={handleSubmit}>{ isLogin ? 'Iniciar sesión' : 'Registrarse' }</button>
+            <h3>ó</h3>
+            <button onClick={loginGoogle}>{ isLogin ? 'Iniciar sesión con Google' : 'Registrarse con Google'}</button>
+            {
+              isLogin
+              ? <h3>¿No tienes cuenta?<span onClick={() => setIsLogin(!isLogin) }> Crea una cuenta</span></h3>
+              : <h3>¿Ya tienes cuenta?<span onClick={() => setIsLogin(!isLogin) }> Iniciar Sesión</span></h3>
+            }
+          </section>
+      }
+    </main>
+  )
+}
+
+export default App
